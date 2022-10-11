33d1a1d60360f8d94e29e7296592f06f19ea8ff1
Brandon Seymour <bseymour122@yahoo.ca>
2022-10-11 16:12:10 -0400

    Add tests to Ship.test.js

---
+++ src/Ship.test.js
@@ -0,0 +1,31 @@
+import Ship from './Ship';
+
+test('Returns the number of hits to a ship', () => {
+  const ship = Ship(3);
+  expect(ship.getHits()).toBe(0);
+});
+
+test('Increases the number of hits', () => {
+  const ship = Ship(4);
+  ship.hit();
+  expect(ship.getHits()).toBe(1);
+});
+
+test('Ship should not be sunk initially', () => {
+  const ship = Ship(3);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test('Ship of length 3 should not sink from 1 hit', () => {
+  const ship = Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
+
+test('Ship of length 3 should sink from 3 hits', () => {
+  const ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
