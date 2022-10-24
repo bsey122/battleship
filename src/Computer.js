de551d47eeaac475b81469b3ed60829972b87e6a
Brandon Seymour <bseymour122@yahoo.ca>
2022-10-23 23:37:45 -0400

    Add Computer factory function

---
+++ src/Computer.js
@@ -0,0 +1,26 @@
+import Player from './Player';
+
+const Computer = (name) => {
+  const { getName, attack } = Player(name);
+  const attackList = [];
+
+  function randomCoordinates() {
+    const x = Math.floor(Math.random() * 11);
+    const y = Math.floor(Math.random() * 11);
+    return { x, y };
+  }
+
+  function autoAttack(coordinates, computerAttack, gameboard) {
+    const { x, y } = coordinates();
+    const isAttacked = attackList.some(
+      (coordinate) => coordinate.x === x && coordinate.y === y
+    );
+    if (!isAttacked) {
+      attackList.push({ x, y });
+    }
+    return computerAttack(x, y, gameboard);
+  }
+
+  return { autoAttack, attack, getName, randomCoordinates, attackList };
+};
+export default Computer;
